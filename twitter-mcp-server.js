29335ec6da09b6b3f7f77346a0b12d8dc4431353
SouravKumarBarman <souravkumarbarman1729@gmail.com>
2025-03-16 13:16:14 +0530

    post tweet funtion done

--- twitter-mcp-server.js
+++ twitter-mcp-server.js
@@ -1,3 +1,6 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import { McpServer, ResourceTemplate } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
